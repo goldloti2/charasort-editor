87a2fb4fcbf08675caaad00514cadd9de26e13c3
goldloti2 <goldloti2@gmail.com>
2025-06-02 23:28:40 +0800

    fix scrollbar not working (#9)

--- view.py
+++ view.py
@@ -1,10 +1,58 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
-from tkinter.constants import CENTER
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from control import Controller
+
+# https://stackoverflow.com/questions/16188420/tkinter-scrollbar-for-frame
+class VerticalScrolledFrame(ttk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame.
+    * Construct and pack/place/grid normally.
+    * This frame only allows vertical scrolling.
+    """
+    def __init__(self, parent, *args, **kw):
+        super().__init__(parent, *args, **kw)
+
+        # Create a canvas object and a vertical scrollbar for scrolling it.
+        vscrollbar = ttk.Scrollbar(self, orient = "vertical")
+        vscrollbar.pack(fill = "y", side = "right", expand = 0)
+        canvas = tk.Canvas(self, bd = 0, highlightthickness = 0,
+                           yscrollcommand = vscrollbar.set)
+        canvas.pack(side = "left", fill = "both", expand = 1)
+        vscrollbar.config(command = canvas.yview)
+
+        # Reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # Create a frame inside the canvas which will be scrolled with it.
+        self.interior = interior = ttk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window = interior,
+                                           anchor = "nw")
+
+        # Track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar.
+        def _configure_interior(event):
+            # Update the scrollbars to match the size of the inner frame.
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion = "0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the canvas's width to fit the inner frame.
+                canvas.config(width = interior.winfo_reqwidth())
+        interior.bind("<Configure>", _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # Update the inner frame's width to fill the canvas.
+                canvas.itemconfigure(interior_id, width = canvas.winfo_width())
+        canvas.bind("<Configure>", _configure_canvas)
+
+        def _on_mousewheel(event):
+            delta = (int)(event.delta / 120)
+            canvas.yview_scroll(-1 * delta, "units")
+        canvas.bind_all("<MouseWheel>", _on_mousewheel)
 
 
 class View:
@@ -23,18 +71,15 @@ class View:
         root.config(menu = menubar)
 
         tabcontrol = ttk.Notebook()
-        flt_tab = ttk.Frame(tabcontrol)
-        chr_tab = ttk.Frame(tabcontrol)
-        tabcontrol.add(flt_tab, text = "Filters")
-        tabcontrol.add(chr_tab, text = "Characters")
+        flt_tab_base = VerticalScrolledFrame(tabcontrol)
+        chr_tab_base = VerticalScrolledFrame(tabcontrol)
+        tabcontrol.add(flt_tab_base, text = "Filters")
+        tabcontrol.add(chr_tab_base, text = "Characters")
         tabcontrol.pack(expand = 1, fill = "both")
 
-        ttk.Scrollbar(flt_tab).pack(side = "right", fill = "y")
-        ttk.Scrollbar(chr_tab).pack(side = "right", fill = "y")
-
         self.root = root
-        self.flt_tab = flt_tab
-        self.chr_tab = chr_tab
+        self.flt_tab = flt_tab_base.interior
+        self.chr_tab = chr_tab_base.interior
         self.flt_frames = []
         self.chr_frames = []
         self.controller = controller
