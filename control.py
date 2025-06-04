40427f0c16ed9583dd9eb6c8d9042e87a7df2c9d
goldloti2 <goldloti2@gmail.com>
2025-06-05 00:25:59 +0800

    combine update_filters and update_characters (#21)

--- control.py
+++ control.py
@@ -14,8 +14,8 @@ class Controller:
     def open_file(self, path="test.js"):
         self.path = path
         self.model = Model(path)
-        self.update_filters()
-        self.update_characters()
+        self.update("filters")
+        self.update("characters")
 
     def save_file(self, path=""):
         if not path:
@@ -26,50 +26,47 @@ class Controller:
 
     def delete(self, index: int, tab: str):
         self.model.delete(index, tab)
-        if tab == "filters":
-            self.update_filters()
-        elif tab == "characters":
-            self.update_characters()
+        self.update(tab)
 
     def move_filter(self, index: int, direction: str):
         self.model.move_filter(index, direction)
-        self.update_filters()
+        self.update("filters")
 
-    def update_filters(self):
-        flt_list = []
-        for flt in self.model.flt_list:
-            node = []
-            node.append(("label", "name:", flt["name"]))
-            node.append(("label", "key:", flt["key"]))
-            node.append(("label", "tooltip:", flt.get("tooltip", "")))
-            node.append(("label", "checked:", flt.get("checked", None)))
-            if "sub" in flt:
-                subs = [("name:", "key:")]
-                for sub in flt["sub"]:
-                    subs.append((sub["name"], sub["key"]))
-                node.append(("sub_frame", "sub:", subs))
-            flt_list.append(node)
-        if flt_list:
-            self.window.refresh_tabs(flt_list, "filters")
+    def update(self, tab: str):
+        if tab == "filters":
+            new_list = [self.gen_node_filter(flt) for flt in self.model.flt_list]
+        elif tab == "characters":
+            new_list = [self.gen_node_character(chr) for chr in self.model.chr_list]
         else:
-            self.window.destroy_tabs("filters")
+            raise ValueError(f"tab '{tab}' not found in controller.update")
+        if new_list:
+            self.window.refresh_tabs(new_list, tab)
+        else:
+            self.window.destroy_tabs(tab)
 
-    def update_characters(self):
-        chr_list = []
-        for chr in self.model.chr_list:
-            node = []
-            node.append(("label", "name:", chr["name"]))
-            node.append(("label", "img:", chr["img"]))
-            opts = [("filter key:", "option:")]
-            chr_opts = chr["opts"]
-            for opt in chr_opts:
-                opts.append((opt, chr_opts[opt]))
-            node.append(("sub_frame", "filter options:", opts))
-            chr_list.append(node)
-        if chr_list:
-            self.window.refresh_tabs(chr_list, "characters")
-        else:
-            self.window.destroy_tabs("character")
+    def gen_node_filter(self, flt: dict):
+        node = []
+        node.append(("label", "name:", flt["name"]))
+        node.append(("label", "key:", flt["key"]))
+        node.append(("label", "tooltip:", flt.get("tooltip", "")))
+        node.append(("label", "checked:", flt.get("checked", None)))
+        if "sub" in flt:
+            subs = [("name:", "key:")]
+            for sub in flt["sub"]:
+                subs.append((sub["name"], sub["key"]))
+            node.append(("sub_frame", "sub:", subs))
+        return node
+
+    def gen_node_character(self, chr: dict):
+        node = []
+        node.append(("label", "name:", chr["name"]))
+        node.append(("label", "img:", chr["img"]))
+        opts = [("filter key:", "option:")]
+        chr_opts = chr["opts"]
+        for opt in chr_opts:
+            opts.append((opt, chr_opts[opt]))
+        node.append(("sub_frame", "filter options:", opts))
+        return node
 
 
 if __name__ == "__main__":
