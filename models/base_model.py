d4f43db079a5d7fc6566f728816894c60425d611
goldloti2 <goldloti2@gmail.com>
2025-06-14 03:22:00 +0800

    done BaseModel.delete/read/update

--- models/base_model.py
+++ models/base_model.py
@@ -1,96 +1,59 @@
-from calmjs.parse import asttypes, es5, io
+from abc import ABC, abstractmethod
+
+from calmjs.parse import asttypes, es5
 from calmjs.parse.walkers import Walker
 
 
-class BaseModel:
-    def __init__(self, path: str):
-        with open(path, "r", encoding="UTF-8") as file:
-            tree = io.read(es5, file)
+class BaseModel(ABC):
+    @staticmethod
+    def update_list(func):
+        def wrapper(self: BaseModel, *args, **kwargs):
+            result = func(self, *args, **kwargs)
 
-        walker = Walker()
-        for node in walker.filter(
-            tree,
-            lambda n: (
-                isinstance(n, asttypes.Assign)
-                and isinstance(n.left, asttypes.DotAccessor)
-            ),
-        ):
-            if node.left.identifier.value == "options":
-                filters = node.right
-            elif node.left.identifier.value == "characterData":
-                characters = node.right
+            def parse(node):
+                if isinstance(node, asttypes.Array):
+                    return [parse(child) for child in node.children()]
+                elif isinstance(node, asttypes.Object):
+                    return {
+                        prop.left.value: parse(prop.right) for prop in node.children()
+                    }
+                else:
+                    return node.value.strip('"')
 
+            self.tree_list = [parse(child) for child in self.tree.children()]
+            return result
+
+        return wrapper
+
+    @update_list
+    def __init__(self, tree: asttypes.Array):
         self.tree = tree
-        self.walker = walker
-        self.filters = filters
-        self.characters = characters
-        self.flt_list = self._tree_to_list(filters)
-        self.chr_list = self._tree_to_list(characters)
+        self.walker = Walker()
+        self.tree_list = []
 
-    def save_file(self, path: str):
-        with open(path, "w") as file:
-            print(self.tree, file=file)
+    @update_list
+    def add(self, record: dict):
+        raise NotImplementedError("BaseModel not implement 'add'")
 
-    def move_filter(self, index: int, direction: int):
-        swap_idx = index + direction
-        swap_list = self.filters.children()
-        swap = swap_list[index]
-        swap_list[index] = swap_list[swap_idx]
-        swap_list[swap_idx] = swap
-        self.flt_list = self._tree_to_list(self.filters)
+    @update_list
+    def delete(self, index: int):
+        self.tree.children().pop(index)
 
-    def delete_record(self, index: int, tab: str):
-        if tab == "filters":
-            self.filters.children().pop(index)
-            self.flt_list = self._tree_to_list(self.filters)
-        elif tab == "characters":
-            self.characters.children().pop(index)
-            self.chr_list = self._tree_to_list(self.characters)
+    def read(self):
+        return self.tree_list
 
-    def update_filter(self, record: dict, index: int):
-        if ("name" not in record) or ("key" not in record):
-            print('filter object require "name" and "key" attribute')
+    @update_list
+    def update(self, record: dict, index: int):
+        js_string = self.parse_input(record)
+        if not js_string:
             return False
-
-        name = record["name"].strip('"')
-        key = record["key"].strip('"')
-        js_string = f'name: "{name}", key: "{key}"'
-
-        if "tooltip" in record:
-            tooltip = record["tooltip"].strip('"')
-            js_string = f'{js_string}, tooltip: "{tooltip}"'
-
-        if "checked" in record:
-            checked = "true" if record["checked"] else "false"
-            js_string = f'{js_string}, checked: "{checked}"'
-
-        if "tree" in record:
-            sub_string = ""
-            for item in record["tree"]:
-                if not item[0] or not item[1]:
-                    print(
-                        '"sub" attribute in filter object require "name" and "key" attribute'
-                    )
-                    return False
-                sub_string = f'{sub_string}, {{name: "{item[0]}", key: "{item[1]}"}}'
-            js_string = f"{js_string}, sub: [{sub_string[2:]}]"
-
         js_string = f"data = {{ {js_string} }}"
         tree = es5(js_string)
         node = next(self.walker.filter(tree, lambda n: isinstance(n, asttypes.Object)))
-        self.filters.children()[index] = node
-        self.flt_list = self._tree_to_list(self.filters)
+        self.tree.children()[index] = node
         return True
 
-    def _tree_to_list(self, tree: asttypes.Array) -> list:
-        def parse_node(node):
-            if isinstance(node, asttypes.Array):
-                return [parse_node(child) for child in node.children()]
-            elif isinstance(node, asttypes.Object):
-                return {
-                    prop.left.value: parse_node(prop.right) for prop in node.children()
-                }
-            else:
-                return node.value.strip('"')
-
-        return [parse_node(child) for child in tree.children()]
+    @classmethod
+    @abstractmethod
+    def parse_input(cls, record: dict) -> str:
+        raise NotImplementedError(f"{cls.__name__} not implement 'parse_input'")
