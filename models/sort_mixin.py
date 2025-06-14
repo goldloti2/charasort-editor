985cd41bde79dace169ed0a0c66ebf33b1381f73
goldloti2 <goldloti2@gmail.com>
2025-06-14 23:43:15 +0800

    rename SortMixin.move_filter to swap for generalization

--- models/sort_mixin.py
+++ models/sort_mixin.py
@@ -3,7 +3,7 @@ from .base_model import BaseModel
 
 class SortMixin:
     @BaseModel.after_db_update
-    def move_filter(self, index: int, direction: int):
+    def swap(self, index: int, direction: int):
         swap_idx = index + direction
         swap_list = self.tree.children()
         swap_list[index], swap_list[swap_idx] = swap_list[swap_idx], swap_list[index]
