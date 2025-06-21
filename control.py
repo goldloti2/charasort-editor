254c13cb2622f3ad5bef58f0b8699a9c38a6c226
goldloti2 <goldloti2@gmail.com>
2025-06-22 02:54:17 +0800

    list handlable errors in controller

--- control.py
+++ control.py
@@ -20,25 +20,39 @@ class Controller:
 
     def open_file(self, path: str):
         self.path = path
-        # TODO
-        self.repo = DataRepository(path)
-        self._update_tab(TabType.FILTERS)
-        self._update_tab(TabType.CHARACTERS)
+        try:
+            self.repo = DataRepository(path)
+        except [FileNotFoundError, OSError]:
+            # TODO: file error, reopen another file
+            pass
+        except [AttributeError, TypeError]:
+            # TODO: AST error, reopen another file
+            pass
+        else:
+            self._update_tab(TabType.FILTERS)
+            self._update_tab(TabType.CHARACTERS)
 
     def save_file(self, path: str = ""):
         if path:
             self.path = path
-        # TODO
-        self.repo.save_file(self.path)
+        try:
+            self.repo.save_file(self.path)
+        except [FileNotFoundError, OSError]:
+            # TODO: can't save the file, let user retry
+            pass
 
     def add_record(self, input_data: InputData, tab: TabType):
         # TODO: implement add, and catch error
         self.repo.add(input_data, tab)
 
     def delete_record(self, index: int, tab: TabType):
-        # TODO
-        self.repo.delete(index, tab)
-        self._update_tab(tab)
+        try:
+            self.repo.delete(index, tab)
+        except IndexError as e:
+            # TODO: index out of bound, just pop up
+            logger.warning(f"{e}, index:{index}")
+        else:
+            self._update_tab(tab)
 
     def update_record(self, input_data: InputData, index: int, tab: TabType):
         try:
@@ -47,23 +61,27 @@ class Controller:
             logger.warning(f"editing {tab.value} #{index} validation failed")
             logger.debug("", exc_info=e)
             raise e
-        except Exception as e:
-            logger.error(f"editing {tab.value} #{index} error", exc_info=e)
-            raise e
         else:
             logger.info(f"editing {tab.value} #{index} success")
             self._update_tab(tab)
 
     def move_filter(self, index: int, direction: int):
-        # TODO
-        self.repo.move_filter(index, direction)
-        self._update_tab(TabType.FILTERS)
+        try:
+            self.repo.move_filter(index, direction)
+        except IndexError as e:
+            # TODO: index out of bound, show message
+            logger.warning(e)
+        else:
+            self._update_tab(TabType.FILTERS)
 
     def _update_tab(self, tab: TabType):
-        # TODO
-        view_list = self.repo.read(tab)
-        # TODO
-        self.window.refresh_tabs(view_list, tab)
+        try:
+            view_list = self.repo.read(tab)
+        except KeyError:
+            # TODO
+            pass
+        else:
+            self.window.refresh_tabs(view_list, tab)
 
 
 if __name__ == "__main__":
